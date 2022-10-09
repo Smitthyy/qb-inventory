0247fd7462381134e1bfc0f9283edd5d80cc61b2
Smitthyy <96345069+Smitthyy@users.noreply.github.com>
2022-10-09 18:39:46 +0100

    Added vehicle class 10 inventory

--- client/main.lua
+++ client/main.lua
@@ -732,6 +732,9 @@ RegisterCommand('inventory', function()
                 elseif vehicleClass == 9 then
                     maxweight = 60000
                     slots = 35
+                elseif vehicleClass == 10 then
+                    maxweight = 4000000
+                    slots = 60
                 elseif vehicleClass == 11 then
                     maxweight = 600000
                     slots = 35
