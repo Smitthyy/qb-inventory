6343ca90a58d8a6092d26343ec7051389820628f
Smithy <96345069+Smitthyy@users.noreply.github.com>
2023-04-08 22:41:36 +0100

    Moved function for MDT to resolve item drop errors

--- server/main.lua
+++ server/main.lua
@@ -1839,44 +1839,7 @@ RegisterNetEvent('inventory:server:SetInventoryData', function(fromInventory, to
 				else
 					QBCore.Functions.Notify(src, Lang:t("notify.nosell"), 'error')
 				end
-			elseif QBCore.Shared.SplitStr(shopType, "_")[1] == "Itemshop" then
-				if Player.Functions.RemoveMoney("cash", price, "itemshop-bought-item") then
-					if QBCore.Shared.SplitStr(itemData.name, "_")[1] == "weapon" then
-						itemData.info.serie = tostring(QBCore.Shared.RandomInt(2) .. QBCore.Shared.RandomStr(3) .. QBCore.Shared.RandomInt(1) .. QBCore.Shared.RandomStr(2) .. QBCore.Shared.RandomInt(3) .. QBCore.Shared.RandomStr(4))
-						itemData.info.quality = 100
-					end
-					local serial = itemData.info.serie
-					local imageurl = ("https://cfx-nui-qb-inventory/html/images/%s.png"):format(itemData.name)
-					local notes = "Purchased at Ammunation"
-					local owner = Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname
-					local weapClass = 1
-					local weapModel = QBCore.Shared.Items[itemData.name].label
-					AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
-					TriggerClientEvent('qb-shops:client:UpdateShop', src, QBCore.Shared.SplitStr(shopType, "_")[2], itemData, fromAmount)
-					QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
-					exports['ps-mdt']:CreateWeaponInfo(serial, imageurl, notes, owner, weapClass, weapModel)
-					TriggerEvent("qb-log:server:CreateLog", "shops", "Shop item bought", "green", "**"..GetPlayerName(src) .. "** bought a " .. itemInfo["label"] .. " for $"..price)
-				elseif bankBalance >= price then
-					Player.Functions.RemoveMoney("bank", price, "itemshop-bought-item")
-					if QBCore.Shared.SplitStr(itemData.name, "_")[1] == "weapon" then
-						itemData.info.serie = tostring(QBCore.Shared.RandomInt(2) .. QBCore.Shared.RandomStr(3) .. QBCore.Shared.RandomInt(1) .. QBCore.Shared.RandomStr(2) .. QBCore.Shared.RandomInt(3) .. QBCore.Shared.RandomStr(4))
-						itemData.info.quality = 100
-					end
-					local serial = itemData.info.serie
-					local imageurl = ("https://cfx-nui-qb-inventory/html/images/%s.png"):format(itemData.name)
-					local notes = "Purchased at Ammunation"
-					local owner = Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname
-					local weapClass = 1
-					local weapModel = QBCore.Shared.Items[itemData.name].label
-					AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
-					TriggerClientEvent('qb-shops:client:UpdateShop', src, QBCore.Shared.SplitStr(shopType, "_")[2], itemData, fromAmount)
-					QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
-					exports['ps-mdt']:CreateWeaponInfo(serial, imageurl, notes, owner, weapClass, weapModel)
-					TriggerEvent("qb-log:server:CreateLog", "shops", "Shop item bought", "green", "**"..GetPlayerName(src) .. "** bought a " .. itemInfo["label"] .. " for $"..price)
-				else
-					QBCore.Functions.Notify(src, "You don't have enough cash..", "error")
-				end
-				else
+			else
 				-- drop
 				toInventory = tonumber(toInventory)
 				if toInventory == nil or toInventory == 0 then
@@ -2147,29 +2110,42 @@ RegisterNetEvent('inventory:server:SetInventoryData', function(fromInventory, to
 				end
 			end
 		elseif QBCore.Shared.SplitStr(shopType, "_")[1] == "Itemshop" then
-			if Player.Functions.RemoveMoney("cash", price, "itemshop-bought-item") then
+            if Player.Functions.RemoveMoney("cash", price, "itemshop-bought-item") then
                 if QBCore.Shared.SplitStr(itemData.name, "_")[1] == "weapon" then
                     itemData.info.serie = tostring(QBCore.Shared.RandomInt(2) .. QBCore.Shared.RandomStr(3) .. QBCore.Shared.RandomInt(1) .. QBCore.Shared.RandomStr(2) .. QBCore.Shared.RandomInt(3) .. QBCore.Shared.RandomStr(4))
-					itemData.info.quality = 100
+                    itemData.info.quality = 100
                 end
-				AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
-				TriggerClientEvent('qb-shops:client:UpdateShop', src, QBCore.Shared.SplitStr(shopType, "_")[2], itemData, fromAmount)
-				QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
-				TriggerEvent("qb-log:server:CreateLog", "shops", "Shop item bought", "green", "**"..GetPlayerName(src) .. "** bought a " .. itemInfo["label"] .. " for $"..price)
-			elseif bankBalance >= price then
-				Player.Functions.RemoveMoney("bank", price, "itemshop-bought-item")
+                local serial = itemData.info.serie
+                local imageurl = ("https://cfx-nui-qb-inventory/html/images/%s.png"):format(itemData.name)
+                local notes = "Purchased at Ammunation"
+                local owner = Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname
+                local weapClass = 1
+                local weapModel = QBCore.Shared.Items[itemData.name].label
+                AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
+                TriggerClientEvent('qb-shops:client:UpdateShop', src, QBCore.Shared.SplitStr(shopType, "_")[2], itemData, fromAmount)
+                QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
+                exports['ps-mdt']:CreateWeaponInfo(serial, imageurl, notes, owner, weapClass, weapModel)
+                TriggerEvent("qb-log:server:CreateLog", "shops", "Shop item bought", "green", "**"..GetPlayerName(src) .. "** bought a " .. itemInfo["label"] .. " for $"..price)
+            elseif bankBalance >= price then
+                Player.Functions.RemoveMoney("bank", price, "itemshop-bought-item")
                 if QBCore.Shared.SplitStr(itemData.name, "_")[1] == "weapon" then
                     itemData.info.serie = tostring(QBCore.Shared.RandomInt(2) .. QBCore.Shared.RandomStr(3) .. QBCore.Shared.RandomInt(1) .. QBCore.Shared.RandomStr(2) .. QBCore.Shared.RandomInt(3) .. QBCore.Shared.RandomStr(4))
-					itemData.info.quality = 100
+                    itemData.info.quality = 100
                 end
-				AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
-				TriggerClientEvent('qb-shops:client:UpdateShop', src, QBCore.Shared.SplitStr(shopType, "_")[2], itemData, fromAmount)
-				QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
-				TriggerEvent("qb-log:server:CreateLog", "shops", "Shop item bought", "green", "**"..GetPlayerName(src) .. "** bought a " .. itemInfo["label"] .. " for $"..price)
-			else
-				QBCore.Functions.Notify(src, "You don't have enough cash..", "error")
-			end
-		else
+                local serial = itemData.info.serie
+                local imageurl = ("https://cfx-nui-qb-inventory/html/images/%s.png"):format(itemData.name)
+                local notes = "Purchased at Ammunation"
+                local owner = Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname
+                local weapClass = 1
+                local weapModel = QBCore.Shared.Items[itemData.name].label
+                AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
+                TriggerClientEvent('qb-shops:client:UpdateShop', src, QBCore.Shared.SplitStr(shopType, "_")[2], itemData, fromAmount)
+                QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
+				exports['ps-mdt']:CreateWeaponInfo(serial, imageurl, notes, owner, weapClass, weapModel)
+                TriggerEvent("qb-log:server:CreateLog", "shops", "Shop item bought", "green", "**"..GetPlayerName(src) .. "** bought a " .. itemInfo["label"] .. " for $"..price)
+            else
+                QBCore.Functions.Notify(src, "You don't have enough cash..", "error")
+            end
 			if Player.Functions.RemoveMoney("cash", price, "unkown-itemshop-bought-item") then
 				AddItem(src, itemData.name, fromAmount, toSlot, itemData.info)
 				QBCore.Functions.Notify(src, itemInfo["label"] .. " bought!", "success")
